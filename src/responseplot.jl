e85db7f67abf1b70fcb60e44329e72d33528f49a
Eliassj <elias.sjolin@gmail.com>
2023-10-11 09:43:46 +0200

    f

--- src/responseplot.jl
+++ src/responseplot.jl
@@ -3,16 +3,19 @@ function responseplot(t::relativeSpikes, period::Int64, depthdiv::Int64=4)
     depthsval::Vector{Float64} = [minimum(r[:, 1]) * i for i in 1:depthdiv]
     fig = Figure()
     axv = Vector{Axis}(undef, depthdiv)
-    xts = append!(append!([-t._specs["back"]], values(t._stimulations)), t._specs["forward"])
+    stimlabs = _tickify(t._stimulations, true)
+    xts = append!(append!([-t._specs["back"]], stimlabs[3]), [t._specs["forward"]])
+    xlabs::Vector{String} = append!(append!([string(-t._specs["back"])], stimlabs[1]), [string(t._specs["forward"])])
     # Create axes
     for n in 1:depthdiv
         if n < depthdiv
+            # If not the bottommost axis...
             axv[n] = Axis(
                 fig[n, 1],
                 xticks=xts,
                 xticksvisible=false,
                 xticklabelsvisible=false,
-                xgridwidth=2.0,
+                xgridwidth=2.5,
                 yticklabelsvisible=false,
                 yticksvisible=false,
                 yticks=[1.0],
@@ -21,10 +24,12 @@ function responseplot(t::relativeSpikes, period::Int64, depthdiv::Int64=4)
             )
             hidespines!(axv[n])
         else
+            # If the bottommost axis...
             axv[n] = Axis(
                 fig[n, 1],
-                xticks=(xts),
+                xticks=(xts, xlabs),
                 xgridwidth=2.0,
+                xlabel="Time (ms)",
                 yticklabelsvisible=false,
                 yticksvisible=false,
                 yticks=[1.0],
@@ -42,7 +47,6 @@ function responseplot(t::relativeSpikes, period::Int64, depthdiv::Int64=4)
     end
     for (i, key) in enumerate(depthsval)
         data = r[findall(x -> x == key, r[:, 1]), :]
-        #data[:, 4] = [hash(data[n, 1], hash(data[n, 2])) for n in eachindex(data[:, 1])] # To sum by time and depth interval
         lines!(
             axv[i], data[:, 2] ./ 30, data[:, 3],
             color=:black,
@@ -51,4 +55,28 @@ function responseplot(t::relativeSpikes, period::Int64, depthdiv::Int64=4)
     end
 
     display(fig)
+    return fig, axv
+end
+
+function _tickify(stimdict::Dict{String,T}, return_all::Bool=false) where {T}
+    vals::Vector{T} = collect(
+        values(stimdict)
+    )
+    names::Vector{String} = collect(
+        Base.keys(stimdict)
+    )
+    srt = sortperm(vals)
+    vals = vals[srt]
+    names = names[srt]
+    out::Vector{String} = Vector(undef, length(vals))
+    n = 1
+    for (key, val) in zip(names, vals)
+        out[n] = string(val) * " (" * key * ")"
+        n += 1
+    end
+    if return_all
+        return out, names, vals
+    else
+        return out
+    end
 end
